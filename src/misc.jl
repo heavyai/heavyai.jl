fee1b8e2e8e0fd3c55b0dfb7af9b12590bea78cd
Randy Zwitch <rzwitch@gmail.com>
2018-06-29 09:25:19 -0400

    Make Int generic
    Specify type as part of implementation, to remove need to write Int32() everywhere. More tests

--- src/misc.jl
+++ src/misc.jl
@@ -79,17 +79,17 @@ clear_gpu_memory(conn::MapDConnection) =
 
 ######################################## query, render
 
-sql_execute(conn::MapDConnection, query::String, column_format::Bool, nonce::String, first_n::Int32, at_most_n::Int32) =
-    sql_execute(conn.c, conn.session, query, column_format, nonce, first_n, at_most_n)
+sql_execute(conn::MapDConnection, query::String, column_format::Bool, nonce::String, first_n::Int, at_most_n::Int) =
+    sql_execute(conn.c, conn.session, query, column_format, nonce, Int32(first_n), Int32(at_most_n))
 
-sql_execute_df(conn::MapDConnection, query::String, device_type::Int32, device_id::Int32, first_n::Int32) =
-    sql_execute_df(conn.c, conn.session, query, device_type, device_id, first_n)
+sql_execute_df(conn::MapDConnection, query::String, device_type::Int, device_id::Int, first_n::Int) =
+    sql_execute_df(conn.c, conn.session, query, Int32(device_type), Int32(device_id), Int32(first_n))
 
-sql_execute_gdf(conn::MapDConnection, query::String, device_id::Int32, first_n::Int32) =
-    sql_execute_gdf(conn.c, conn.session, query, device_id, first_n)
+sql_execute_gdf(conn::MapDConnection, query::String, device_id::Int, first_n::Int) =
+    sql_execute_gdf(conn.c, conn.session, query, Int32(device_id), Int32(first_n))
 
-deallocate_df(conn::MapDConnection, df::TDataFrame, device_type::Int32, device_id::Int32) =
-    deallocate_df(conn.c, conn.session, df, device_type, device_id)
+deallocate_df(conn::MapDConnection, df::TDataFrame, device_type::Int, device_id::Int) =
+    deallocate_df(conn.c, conn.session, df, Int32(device_type), Int32(device_id))
 
 interrupt(conn::MapDConnection) =
     interrupt(conn.c, conn.session)
@@ -99,22 +99,22 @@ interrupt(conn::MapDConnection) =
 sql_validate(conn::MapDConnection, query::String) =
     sql_validate(conn.c, conn.session, query)
 
-get_completion_hints(conn::MapDConnection, sql::String, cursor::Int32) =
-    get_completion_hints(conn.c, conn.session, sql, cursor)
+get_completion_hints(conn::MapDConnection, sql::String, cursor::Int) =
+    get_completion_hints(conn.c, conn.session, sql, Int32(cursor))
 
-set_execution_mode(conn::MapDConnection, mode::Int32) =
-    set_execution_mode(conn.c, conn.session, mode)
+set_execution_mode(conn::MapDConnection, mode::Int) =
+    set_execution_mode(conn.c, conn.session, Int32(mode))
 
-render_vega(conn::MapDConnection, widget_id::Int64, vega_json::String, compression_level::Int32, nonce::String) =
-    render_vega(conn.c, conn.session, widget_id, vega_json, compression_level, nonce)
+render_vega(conn::MapDConnection, widget_id::Int, vega_json::String, compression_level::Int, nonce::String) =
+    render_vega(conn.c, conn.session, Int64(widget_id), vega_json, Int32(compression_level), nonce)
 
-get_result_row_for_pixel(conn::MapDConnection, widget_id::Int64, pixel::TPixel, table_col_names::Dict{String,Vector{String}}, column_format::Bool, pixelRadius::Int32, nonce::String) =
-    get_result_row_for_pixel(conn.c, conn.session, widget_id, pixel, table_col_names, column_format, pixelRadius, nonce)
+get_result_row_for_pixel(conn::MapDConnection, widget_id::Int, pixel::TPixel, table_col_names::Dict{String,Vector{String}}, column_format::Bool, pixelRadius::Int, nonce::String) =
+    get_result_row_for_pixel(conn.c, conn.session, Int64(widget_id), pixel, table_col_names, column_format, Int32(pixelRadius), nonce)
 
 ######################################## dashboard
 
-get_dashboard(conn::MapDConnection, dashboard_id::Int32) =
-    get_dashboard(conn.c, conn.session, dashboard_id)
+get_dashboard(conn::MapDConnection, dashboard_id::Int) =
+    get_dashboard(conn.c, conn.session, Int32(dashboard_id))
 
 get_dashboards(conn::MapDConnection) =
     get_dashboards(conn.c, conn.session)
@@ -122,20 +122,20 @@ get_dashboards(conn::MapDConnection) =
 create_dashboard(conn::MapDConnection, dashboard_name::String, dashboard_state::String, image_hash::String, dashboard_metadata::String) =
     create_dashboard(conn.c, conn.session, dashboard_name, dashboard_state, image_hash, dashboard_metadata)
 
-replace_dashboard(conn::MapDConnection, dashboard_id::Int32, dashboard_name::String, dashboard_owner::String, dashboard_state::String, image_hash::String, dashboard_metadata::String) =
-    replace_dashboard(conn.c, conn.session, dashboard_id, dashboard_name, dashboard_owner, dashboard_state, image_hash, dashboard_metadata)
+replace_dashboard(conn::MapDConnection, dashboard_id::Int, dashboard_name::String, dashboard_owner::String, dashboard_state::String, image_hash::String, dashboard_metadata::String) =
+    replace_dashboard(conn.c, conn.session, Int32(dashboard_id), dashboard_name, dashboard_owner, dashboard_state, image_hash, dashboard_metadata)
 
-delete_dashboard(conn::MapDConnection, dashboard_id::Int32) =
-    delete_dashboard(conn.c, conn.session, dashboard_id)
+delete_dashboard(conn::MapDConnection, dashboard_id::Int) =
+    delete_dashboard(conn.c, conn.session, Int32(dashboard_id))
 
-share_dashboard(conn::MapDConnection, dashboard_id::Int32, groups::Vector{String}, objects::Vector{String}, permissions::TDashboardPermissions) =
-    share_dashboard(conn.c, conn.session, dashboard_id, groups, objects, permissions)
+share_dashboard(conn::MapDConnection, dashboard_id::Int, groups::Vector{String}, objects::Vector{String}, permissions::TDashboardPermissions) =
+    share_dashboard(conn.c, conn.session, Int32(dashboard_id), groups, objects, permissions)
 
-unshare_dashboard(conn::MapDConnection, dashboard_id::Int32, groups::Vector{String}, objects::Vector{String}, permissions::TDashboardPermissions) =
-    unshare_dashboard(conn.c, conn.session, dashboard_id, groups, objects, permissions)
+unshare_dashboard(conn::MapDConnection, dashboard_id::Int, groups::Vector{String}, objects::Vector{String}, permissions::TDashboardPermissions) =
+    unshare_dashboard(conn.c, conn.session, Int32(dashboard_id), groups, objects, permissions)
 
-get_dashboard_grantees(conn::MapDConnection, dashboard_id::Int32) =
-    get_dashboard_grantees(conn.c, conn.session, dashboard_id)
+get_dashboard_grantees(conn::MapDConnection, dashboard_id::Int) =
+    get_dashboard_grantees(conn.c, conn.session, Int32(dashboard_id))
 
 ######################################## dashboard links
 
@@ -162,8 +162,8 @@ load_table(conn::MapDConnection, table_name::String, rows::Vector{TStringRow}) =
 detect_column_types(conn::MapDConnection, file_name::String, copy_params::TCopyParams) =
     detect_column_types(conn.c, conn.session, file_name, copy_params)
 
-create_table(conn::MapDConnection, table_name::String, row_desc::TRowDescriptor, table_type::Int32) =
-    create_table(conn.c, conn.session, table_name, row_desc, table_type)
+create_table(conn::MapDConnection, table_name::String, row_desc::TRowDescriptor, table_type::Int) =
+    create_table(conn.c, conn.session, table_name, row_desc, Int32(table_type))
 
 import_table(conn::MapDConnection, table_name::String, file_name::String, copy_params::TCopyParams) =
     import_table(conn.c, conn.session, table_name, file_name, copy_params)
@@ -182,8 +182,8 @@ get_roles(conn::MapDConnection) =
 get_db_objects_for_grantee(conn::MapDConnection, roleName::String) =
     get_db_objects_for_grantee(conn.c, conn.session, roleName)
 
-get_db_object_privs(conn::MapDConnection, objectName::String, _type::Int32) =
-    get_db_object_privs(conn.c, conn.session, objectName, _type)
+get_db_object_privs(conn::MapDConnection, objectName::String, type_::Int) =
+    get_db_object_privs(conn.c, conn.session, objectName, Int32(type_))
 
 get_all_roles_for_user(conn::MapDConnection, userName::String) =
     get_all_roles_for_user(conn.c, conn.session, userName)
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,8 @@ end
 #wrap in ifexists before using on Travis
 include("credentials.jl")
 
+
+######################################## connection, admin
 #connect to database
 conn = MapD.connect(host, port , user, passwd, dbname)
 @test typeof(conn) == MapD.MapDConnection
@@ -28,21 +30,85 @@ ptables = get_physical_tables(conn)
 views = get_views(conn)
 @test typeof(views) == Vector{String}
 
+met = get_tables_meta(conn)
+@test typeof(met) == Vector{MapD.TTableMeta}
 
-# met = get_tables_meta(conn)
-# table_deet = get_table_details(conn, "fordgobike_tripdata_v2")
-# #int_tab_deet = get_internal_table_details(conn, "")
-# users = get_users(conn)
-# databases = get_databases(conn)
-# version = get_version(conn)
-# clear_cpu = clear_cpu_memory(conn)
-# clear_gpu = clear_gpu_memory(conn)
-# interupt = interrupt(conn)
-# frontend_views = get_frontend_views(conn)
-# dashboards = get_dashboards(conn)
-# mem = get_memory(conn, "cpu") # "cpu" or "gpu" based on ThriftWithRetry.h
-#
-#
-#
-# #disconnect from database
-# disc = disconnect(conn)
+#Need to change eventually, depending on what is in test instance
+table_deet = get_table_details(conn, "fordgobike_tripdata_v2")
+@test typeof(table_deet) == MapD.TTableDetails
+
+users = get_users(conn)
+@test typeof(users) == Vector{String}
+
+databases = get_databases(conn)
+@test typeof(databases) == Vector{MapD.TDBInfo}
+
+version = get_version(conn)
+@test typeof(version) == String
+
+#start_heap_profile
+#stop_heap_profile
+#get_heap_profile
+
+mem = get_memory(conn, "cpu")
+@test typeof(mem) == Vector{MapD.TNodeMemoryInfo}
+
+clear_cpu = clear_cpu_memory(conn)
+clear_gpu = clear_gpu_memory(conn)
+
+######################################## query, render
+
+#sql_execute
+#sql_execute_df
+sdf = sql_execute_df(conn, "select count(*) as records from fordgobike_tripdata_v2", 0, 0, 1000)
+#sql_execute_gdf
+#deallocate_df
+#interrupt
+
+sqlval = sql_validate(conn, "select count(*) as records from fordgobike_tripdata_v2")
+@test typeof(sqlval) == Dict{String,MapD.TColumnType}
+
+#get_completion_hints
+#set_execution_mode(conn, GPU)
+#render_vega
+#get_result_row_for_pixel
+
+######################################## dashboard
+
+#get_dashboard
+#get_dashboards
+#create_dashboard
+#replace_dashboard
+#delete_dashboard
+#share_dashboard
+#unshare_dashboard
+#get_dashboard_grantees
+
+######################################## dashboard links
+
+#get_link_view
+#create_link
+
+######################################## import
+
+#load_table_binary
+#load_table_binary_columnar
+#load_table_binary_arrow
+#load_table
+
+######################################## object privileges
+
+#get_roles
+#get_db_objects_for_grantee
+#get_db_object_privs
+#get_all_roles_for_user
+
+######################################## licensing
+
+#set_license_key
+#get_license_claims
+
+
+
+#disconnect from database
+disc = disconnect(conn)
