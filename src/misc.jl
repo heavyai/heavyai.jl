2b771f0bd500a5cb01c01181ad8d12c0129281ac
randyzwitch <rzwitch@gmail.com>
2018-10-04 13:03:49 -0400

    Clean up load_buffer arguments

--- src/misc.jl
+++ src/misc.jl
@@ -26,22 +26,22 @@ function connect(host::String, port::Int, user::String, passwd::String, dbname::
 
 end
 
-function load_buffer(handle::Vector{UInt8}, size::Int)
+function load_buffer(handle::Vector{UInt8}, size::Int, own::Bool=false)
 
     # get key as UInt32, then get string value instead of len 1 array
     shmkey = reinterpret(UInt32, handle)[1]
 
     # use <sys/ipc.h> from C standard library to get shared memory id
     # validate that shmget returns a valid id
-    shmid = ccall((:shmget, "libc"), Cint, (Cuint, Int32, Int32), shmkey, size, 0)
+    shmid = ccall(:shmget, Cint, (Cuint, Int32, Int32), shmkey, size, 0)
     shmid == -1 ? error("Invalid shared memory key: $shmkey") : nothing
 
     # with shmid, get shared memory start address
-    ptr = ccall((:shmat, "libc"), Ptr{Nothing}, (Cint, Ptr{Nothing}, Cint), shmid, C_NULL, 0)
+    ptr = ccall(:shmat, Ptr{Nothing}, (Cint, Ptr{Nothing}, Cint), shmid, C_NULL, 0)
 
     # makes a zero-copy reference to memory, true gives ownership to julia
     # validate that memory no longer needs to be released using MapD methods
-    return unsafe_wrap(Array, convert(Ptr{UInt8}, ptr), size)
+    return unsafe_wrap(Array, convert(Ptr{UInt8}, ptr), size, own = own)
 
 end
 
