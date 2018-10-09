30f455a31d5b216ae635b0f163ab0e83a2b4a16c
Randy Zwitch <rzwitch@gmail.com>
2018-10-09 16:45:45 -0400

    Autobuild docs based on docstrings (#18)

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter, OmniSci
+
+makedocs(
+    # options
+    modules = [OmniSci],
+    format = :html,
+    sitename = "OmniSci.jl",
+    pages = [
+        "index.md"
+
+    ]
+)
+
+deploydocs(
+    repo = "github.com/omnisci/OmniSci.jl.git",
+    julia = "1.0"
+)
