8871c840fd4a333162825685fa69d4a42803966e
Randy Zwitch <rzwitch@gmail.com>
2018-10-24 16:01:56 -0700

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -101,8 +101,9 @@ ddash = delete_dashboard(conn, cdash)
 
 #get roles assigned to user
 sql_execute(conn, "create role testuser") #TODO: write convenience method when result unlikely to return a result instead of empty dataframe
-sql_execute(conn, "grant testuser to mapd")
-roleuser = get_all_roles_for_user(conn, "mapd") #TODO: tests fail here
+sql_execute(conn, "create user mapd2")
+sql_execute(conn, "grant testuser to mapd2")
+roleuser = get_all_roles_for_user(conn, "mapd2") #TODO: tests fail here
 @test typeof(roleuser) == Vector{String}
 
 gobj = get_db_objects_for_grantee(conn, "testuser")
