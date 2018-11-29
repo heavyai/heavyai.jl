38042fbb5e24aba54c4a8abc9b256542df04296b
Randy Zwitch <rzwitch@gmail.com>
2018-11-29 15:33:02 -0500

    Parse enums in get_table_details (#48)
    Fixes #46

--- src/OmniSci.jl
+++ src/OmniSci.jl
@@ -1,6 +1,6 @@
 module OmniSci
 
-using Thrift, Random, Sockets, Distributed, DataFrames, Dates
+using Thrift, Random, Sockets, Distributed, DataFrames, Dates, DecFP, GeoInterface
 import Thrift: process, meta, distribute, ThriftMeta, ThriftMetaAttribs
 import Base: show
 import DataFrames: DataFrame
