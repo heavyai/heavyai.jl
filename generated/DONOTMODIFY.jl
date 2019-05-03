80606124aab3de1c9e7ca999c9eaef91121c57e7
randyzwitch <rzwitch@gmail.com>
2019-05-03 17:35:16 -0400

    Remove old generated code

--- generated/DONOTMODIFY.jl
+++
@@ -1,5 +0,0 @@
-#The files are here for documenting the output directly from Thrift.jl.
-#Do not modify the code here, unless the bindings were re-generated
-
-#https://github.com/omnisci/mapd-core/commit/324547f4b0b5082f094099065429fb321a41c7b8
-#Need to change Service MapD line inside file to ensure julia doesn't overwrite during gen process
